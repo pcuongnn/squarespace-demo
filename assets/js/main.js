968dba6cd9abd60fe8f03687f8b1ed77b38fbc54
pcuong <pcuongnn@gmail.com>
2015-09-21 15:05:09 +0700

    update-css

---
+++ assets/js/main.js
@@ -0,0 +1,67 @@
+jQuery(function(){
+	//////////////////////////////////////////////////////////////////////////	
+    // scrollto
+    //////////////////////////////////////////////////////////////////////////		
+	jQuery(".scrollto").click(function(T){
+		T.preventDefault();
+		var Q=jQuery(this).attr("name");
+		jQuery("html,body").clearQueue().animate({
+			scrollTop:jQuery('#'+Q).offset().top
+		},1800,"swing");
+		return false;
+	});
+	if (jQuery(window).width() <= 1200 ) {
+		$("#menu").mmenu({
+			extensions: ["pageshadow", "theme-white"],
+			counters: false
+		}).on('click',
+			'a[href^="#/"]',
+			function () {
+				alert("Thank you for clicking, but that's a demo link.");
+				return false;
+			}
+		);
+	}
+
+	jQuery(window).on("load", function() {
+		jQuery('.flexslider').flexslider({
+			controlNav: false,
+			directionNav: false,
+			slideshow: true,
+			animationSpeed: 800,
+			multipleKeyboard: true,
+			pauseOnHover: true
+		});
+
+		$('#sliders .slide-prev, #sliders .slide-next').on('click', function(){
+			var href = $(this).attr('href');
+			$('.flexslider').flexslider(href);
+			return false;
+		});
+	});
+
+	jQuery('.feed-inner ul').bxSlider({
+		mode: 'fade',
+		auto: true,
+		controls: false,
+		pager: false,
+		captions: false
+	});
+
+	jQuery('.s-menu li').find('ul').parent().addClass('has-child');
+	jQuery('.has-child a').click(function(e){
+		e.preventDefault();
+		if (jQuery(this).hasClass('opened')) {
+			jQuery(this).parent().find('ul').slideUp('slow');
+			jQuery(this).removeClass('opened');
+		} else {
+			jQuery(this).parent().find('ul').slideDown('slow');
+			jQuery(this).addClass('opened');
+		}
+	});
+	jQuery(".s-menu li a[href='" + window.location.pathname + "']").addClass('active');
+	jQuery(".s-menu li ul li a[href='" + window.location.pathname + "']").addClass('active');
+	jQuery('.s-menu li li a').each(function(){
+		if(jQuery(this).hasClass('active')){jQuery(this).parents('ul').show();}
+	});
+});
