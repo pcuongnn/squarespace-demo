e658d6e8d1adb707e11b65ed16956f7814cf0250
pcuong <pcuongnn@gmail.com>
2015-09-25 13:19:25 +0700

    update-template

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,20 @@
 jQuery(function(){
+	//////////////////////////////////////////////////////////////////////////
+	// Tab
+	//////////////////////////////////////////////////////////////////////////
+	var $tabsNav = jQuery('.tabs-nav'),
+		$tabsNavLis = $tabsNav.children('li');
+	$tabsNav.each(function () {
+		var $this = jQuery(this);
+		$this.parent().prev().children('.tab-content').stop(true, true).hide().first().show();
+		$this.children('li').first().addClass('active').stop(true, true).show();
+	});
+	$tabsNavLis.on('click', function (e) {
+		var $this = jQuery(this);
+		$this.siblings().removeClass('active').end().addClass('active');
+		$this.parent().parent().prev().children('.tab-content').stop(true, true).hide().siblings($this.find('a').attr('href')).fadeIn();
+		e.preventDefault();
+	});
 	//////////////////////////////////////////////////////////////////////////	
     // scrollto
     //////////////////////////////////////////////////////////////////////////		
@@ -64,4 +80,5 @@ jQuery(function(){
 	jQuery('.s-menu li li a').each(function(){
 		if(jQuery(this).hasClass('active')){jQuery(this).parents('ul').show();}
 	});
+
 });
